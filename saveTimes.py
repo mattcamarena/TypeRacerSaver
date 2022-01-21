9cc92a9cd7d8405d3b55bae97bb72d2110576e0d
Matthew Camarena <camarena.matthew@gmail.com>
2022-01-20 18:28:12 -0600

    Update saveTimes.py
    cleaned up code a bit & added comments

--- saveTimes.py
+++ saveTimes.py
@@ -1,40 +1,47 @@
+# Python Script to Webscrap Data from User's Typeracer profile
+
+# Imports
 import requests
 import re
 from bs4 import BeautifulSoup
 from datetime import datetime
 
+# Initializing Variables
 dt = datetime.now()
 WPM = []
-percents = []
+accuracy = []
 dates = []
 runningAvg = []
-# if I want to choose the username
+raceCount = 0
+
+# Ask for Username to run the script on 
 usr = input("Username: ")
 
+# Get the Number of Races to set the GET URL
 print("Getting Number of Races...")
-# first reponse to get number of races to use on second request
 response = requests.get("https://data.typeracer.com/pit/profile?user="+ usr)
 soup = BeautifulSoup(response.text, 'html.parser')
 getRaceCount = soup.findAll('span', {"class": "Stat__Top"})
 raceCount = getRaceCount[2].text
 
+# Get HTML page with all the Race Data
 print("Getting Race Information")
 response = requests.get("https://data.typeracer.com/pit/race_history?user="+usr+"&n=" + raceCount + "&startDate=&universe=")
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# Setting up times and percents variables
-times = soup.findAll('div', attrs={"class":"profileTableHeaderRaces"})
+# Getting WPM and Accuracy
+titles = soup.findAll('div', attrs={"class":"profileTableHeaderRaces"})
 my_flag = True
-for title in times:
+for title in titles:
 	if my_flag:
 		WPM.insert(0, title.text.strip())
 		my_flag = not my_flag
 	else:
-		percents.insert(0, title.text.strip())
+		accuracy.insert(0, title.text.strip())
 		my_flag = not my_flag
-# dates will need fix today into date...
-getDates = soup.findAll('div', attrs={"class":"profileTableHeaderDate"})
 
+# Getting dates for each races
+getDates = soup.findAll('div', attrs={"class":"profileTableHeaderDate"})
 for mdate in getDates:
 	if(mdate.text.strip() == 'today'):
 		dates.insert(0, dt.strftime('%b. %d, %Y'))
@@ -49,11 +56,8 @@ for i in range(len(dates)):
 	rsum += int(WPM[i])
 	runningAvg.append(int(rsum/(i+1)))
 
-
-
-print("Writing to text file")
 # write to txt file
+print("Writing to text file")
 with open('scores.txt', 'w') as f:
 	for i in range(len(WPM)):
-		f.write(dates[i]+","+WPM[i]+","+str(runningAvg[i])+","+percents[i]+"\n")
-
+		f.write(dates[i]+","+WPM[i]+","+str(runningAvg[i])+","+accuracy[i]+"\n")
